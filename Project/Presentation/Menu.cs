8df07cbfebc856b434801a4576a6ac3be1244fae
mrcoldshower <113611227+mrcoldshower@users.noreply.github.com>
2023-03-29 13:53:19 +0200

    Update Menu.cs

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -1,27 +1,40 @@
 static class Menu
 {
-
+    public static string message = "";
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
     static public void Start()
     {
+        Console.Clear();
+        if (message != "") Console.WriteLine(message + "\n");
+        message = "";
         Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to view all available food products");
+        Console.WriteLine("Enter 2 to create an account");
+        Console.WriteLine("Enter 3 to see the menu card");
+        Console.WriteLine("Enter 4 to exit the program");
 
-        string input = Console.ReadLine()!;
+        string input = Console.ReadLine() ?? "";
         if (input == "1")
         {
             UserLogin.Start();
         }
         else if (input == "2")
         {
+            UserLogin.CreateAccount();
+        }
+        else if (input == "3")
+        {
             MenuCard.ShowMenuCard();
-            Console.ReadLine();
+        }
+        else if (input == "4")
+        {
+            System.Environment.Exit(0);
         }
         else
         {
-            Console.WriteLine("Invalid input");
+            message = "Invalid input";
+            Console.Clear();
             Start();
         }
 
