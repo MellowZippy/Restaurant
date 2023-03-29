73995cf90f3ba681f41fc6808de59766be4b34c8
ItzHarold <114648392+ItzHarold@users.noreply.github.com>
2023-03-29 14:07:18 +0200

    Update Menu.cs

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -1,41 +1,107 @@
 static class Menu
 {
-    public static string message = "";
+
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
+    static public void AdminUI()
+    {
+        Console.WriteLine("Enter 1 to see all reservations");
+        Console.WriteLine("Enter 2 to see Menu");
+        Console.WriteLine("Enter 3 to change the Menu");
+        Console.WriteLine("Enter 4 to change Restaurant Layout");
+        Console.WriteLine("Enter 5 to change Restaurant Information");
+        Console.WriteLine("Enter 6 to Quit");
+        string input = Console.ReadLine()!;
+            if (input == "1")
+            {
+                //Method to see all Reservation
+            }
+            else if (input == "2")
+            {
+                MenuCard.ShowMenuCard();
+            }
+            else if (input == "3")
+            {
+                //Method to change Menu
+            }
+            else if (input == "4")
+            {
+                //Method to change Restaurant layout
+            }
+            else if (input == "5")
+            {
+                //Method to change Restaurant Information
+            }
+            else if (input == "6")
+            {
+                System.Environment.Exit(0);
+            }
+            {
+                Console.WriteLine("Invalid input");
+                Menu.AdminUI();
+            }
+    }
+
+    static public void UserUI()
+    {
+        Console.WriteLine("Enter 1 to see your Reservation");
+        Console.WriteLine("Enter 2 to make your Reservation");
+        Console.WriteLine("Enter 3 to change your Reservation");
+        Console.WriteLine("Enter 4 to remove your Reservation");
+        string input = Console.ReadLine()!;
+            if (input == "1")
+            {
+                //Method to see all reservation
+            }
+            else if (input == "2")
+            {
+                //Method to make your reservation
+            }
+            else if (input == "3")
+            {
+                //Method to change your reservation
+            }
+            else if (input == "4")
+            {
+                //Method to remove your reservation
+            }
+                Console.WriteLine("Invalid input");
+                Menu.AdminUI();
+            
+    }
+
+    static public void WaiterUI()
+    {
+        Console.WriteLine("hello waiter");
+    }
+
+
+
     static public void Start()
     {
-        Console.Clear();
-        if (message != "") Console.WriteLine(message + "\n");
-        message = "";
         Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to create an account");
-        Console.WriteLine("Enter 3 to see the menu card");
-        Console.WriteLine("Enter 4 to exit the program");
+        Console.WriteLine("Enter 2 to view Menu");
+        Console.WriteLine("Enter 3 to Quit");
 
-        string input = Console.ReadLine() ?? "";
+        string input = Console.ReadLine()!;
         if (input == "1")
         {
             UserLogin.Start();
         }
         else if (input == "2")
         {
-            UserLogin.CreateAccount();
+            MenuCard.ShowMenuCard();
+            Console.ReadLine();
         }
         else if (input == "3")
-        {
-            MenuCard.ShowMenuCard();
-        }
-        else if (input == "4")
         {
             System.Environment.Exit(0);
         }
         else
         {
-            message = "Invalid input";
-            Console.Clear();
-            Start();
+            Console.WriteLine("Invalid input");
+            Menu.Start();
         }
 
     }
