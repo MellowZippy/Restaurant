26d56b587609cdcdcda15b9e48074bb5ab4811ef
mrcoldshower <113611227+mrcoldshower@users.noreply.github.com>
2023-03-29 13:50:31 +0200

    Create ReservationModel.cs

---
+++ Project/DataModels/ReservationModel.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+
+public class ReservationModel
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("date")]
+    public DateTime Date { get; set; }
+
+    [JsonPropertyName("time")]
+    public string Time { get; set; }
+
+    [JsonPropertyName("quantityPeople")]
+    public int QuantityPeople { get; set; }
+
+    [JsonPropertyName("fullName")]
+    public string FullName { get; set; }
+
+
+
+    public ReservationModel(int id, DateTime date, string time, int quantityPeople, string fullName)
+    {
+        Id = id;
+        Date = date;
+        Time = time;
+        QuantityPeople = quantityPeople;
+        FullName = fullName;
+    }
+
+}
+
+
+
